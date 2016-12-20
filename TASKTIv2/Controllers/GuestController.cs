5790e2d96454aa1f191720c6b095e2574e5fd817
KEVIN <KEVIN@DESKTOP-GJRP3IL>
2016-12-20 21:09:13 +0700

    Add project files.

---
+++ TASKTIv2/Controllers/GuestController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using TASKTIv2.DAL;
+using TASKTIv2.Models;
+
+namespace TASKTIv2.Controllers
+{
+    public class GuestController : Controller
+    {
+        // GET: Guest
+        public ActionResult Index()
+        {
+            using (GuestDAL service = new GuestDAL())
+            {
+                var model = service.GetData().ToList();
+                if (TempData["Pesan"] != null)
+                {
+                    ViewBag.Pesan = TempData["Pesan"].ToString();
+                }
+                return View(model);
+            }
+        }
+
+        public ActionResult IndexGuest()
+        {
+            string Id = Session["IdGuest"].ToString();
+            using (GuestDAL service = new GuestDAL())
+            {
+                return View(service.GetDataByUsername(Id).ToList());
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(DataGuest guest)
+        {
+            using (GuestDAL service = new GuestDAL())
+            {
+                try
+                {
+                    service.Add(guest);
+                    TempData["Pesan"] = Helpers.KotakPesan.GetPesan("Sukses !",
+                        "success", "Data Guest " + guest.NamaGuest + " berhasil ditambah");
+                }
+                catch (Exception ex)
+                {
+                    TempData["Pesan"] = Helpers.KotakPesan.GetPesan("Error !",
+                                          "danger", ex.Message);
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            using (GuestDAL services = new GuestDAL())
+            {
+                var result = services.GetDataById(id);
+                return View(result);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(DataGuest guest)
+        {
+            using (GuestDAL services = new GuestDAL())
+            {
+                try
+                {
+                    services.Edit(guest);
+                    TempData["Pesan"] = Helpers.KotakPesan.GetPesan("Sukses !",
+                        "success", "Data Author " + guest.NamaGuest + " berhasil diedit");
+                }
+                catch (Exception ex)
+                {
+                    TempData["Pesan"] = Helpers.KotakPesan.GetPesan("Error !",
+                                         "danger", ex.Message);
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Delete(int id)
+        {
+            using (GuestDAL service = new GuestDAL())
+            {
+                try
+                {
+                    service.Delete(id);
+                    TempData["Pesan"] = Helpers.KotakPesan.GetPesan("Sukses !",
+                        "success", "Data Author berhasil didelete !");
+                }
+                catch (Exception ex)
+                {
+                    TempData["Pesan"] = Helpers.KotakPesan.GetPesan("Error !",
+                                         "danger", ex.Message);
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Search(string txtSearch)
+        {
+            using (GuestDAL svCat = new GuestDAL())
+            {
+                var results = svCat.Search(txtSearch).ToList();
+                return View("Index", results);
+            }
+        }
+    }
+}
---
+++ TASKTIv2/DAL/GuestDAL.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Dapper;
+using TASKTIv2.Models;
+
+namespace TASKTIv2.DAL
+{
+    public class GuestDAL : IDisposable
+    {
+        private GuestModel db = new GuestModel();
+
+
+        private string GetConnstr()
+        {
+            return ConfigurationManager.ConnectionStrings["GuestModel"].ConnectionString;
+        }
+        public IQueryable<DataGuest> GetData()
+        {
+            var results = from a in db.DataGuests
+                          orderby a.NamaGuest
+                          select a;
+            return results;
+        }
+
+        public DataGuest GetDataById(int id)
+        {
+            var result = (from a in db.DataGuests
+                          where a.IdGuest == id
+                          select a).SingleOrDefault();
+
+            return result;
+        }
+
+
+        public IQueryable<DataGuest> GetDataByUsername(string NameGuest)
+        {
+            var result = from a in db.DataGuests
+                         where a.NamaGuest == NameGuest
+                         select a;
+
+            return result;
+        }
+        public void Add(DataGuest obj)
+        {
+            try
+            {
+                db.DataGuests.Add(obj);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void Edit(DataGuest obj)
+        {
+            var model = GetDataById(obj.IdGuest);
+
+            if (model != null)
+            {
+                model.NamaGuest = obj.NamaGuest;
+                model.Alamat = obj.Alamat;
+                model.Ucapan = obj.Ucapan;
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+            else
+            {
+                throw new Exception("Data tidak ditemukan !");
+            }
+        }
+
+        public void Delete(int id)
+        {
+            var model = GetDataById(id);
+            if (model != null)
+            {
+                try
+                {
+                    db.DataGuests.Remove(model);
+                    db.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message, ex.InnerException);
+                }
+            }
+            else
+            {
+                throw new Exception("Data tidak ditemukan !");
+            }
+        }
+
+        public IQueryable<DataGuest> Search(string txtsearch)
+        {
+            var results = from m in db.DataGuests
+                          where m.NamaGuest.ToLower().Contains(txtsearch.ToLower())
+                          select m;
+            return results;
+
+        }
+
+        //public IEnumerable<DataGuest> Search(string txtsearch)
+        //{
+        //    using (SqlConnection conn = new SqlConnection(GetConnstr()))
+        //    {
+        //        string strsql = @"select * from DataGuest where NamaGuest='" + txtsearch + "'";
+
+
+        //        return conn.Query<DataGuest>(strsql);
+        //    }
+        //}
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+    }
+}
